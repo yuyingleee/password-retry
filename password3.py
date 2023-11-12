01f8ed856728c7d277a612e57a60f01bf7f0cdf0
Yuying Lee <yyleee@gmail.com>
2023-11-13 02:58:04 +0800

    answer

---
+++ password3.py
@@ -0,0 +1,12 @@
+password = 'a123456'
+i = 3 #剩餘機會
+while i > 0:
+	i = i - 1
+	pwd = input('請輸入密碼：')
+	if pwd == password:
+		print('登入成功！')
+		break
+	else:
+		print('密碼錯誤！')
+		if i > 0:
+			print('還有', i , '次機會')
